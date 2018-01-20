5ba895ab25c41f4afe97036cf3a2d383e41b393d
givologyva <givologyva@gmail.com>
2018-01-19 23:21:04 -0500

    xcode

---
+++ PhoneBook/PhoneBook/Contact.swift
@@ -0,0 +1,15 @@
+//
+//  Contact.swift
+//  PhoneBook
+//
+//  Created by Vishnu Murthy on 1/19/18.
+//  Copyright © 2018 Vishnu Murthy. All rights reserved.
+//
+
+import Foundation
+
+struct Contact
+{
+    let name: String
+    let phone: String
+}
